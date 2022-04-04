9e69b0847b9484e663add6d926f7caf30afbd179
MateusBeta <mateusbeta99@hotmail.com>
2022-04-04 08:52:35 -0300

    Iniciando no c++

---
+++ 1-01042022/primeiro.cpp
@@ -0,0 +1,10 @@
+//Primeiro Programa em c++
+
+#include <iostream>
+
+int main() {
+        //cout(console out) -> Saída na tela
+    std::cout << "Primeiro programa em c++\n";
+    return 0;
+
+}
---
+++ 1-01042022/segundo.cpp
@@ -0,0 +1,16 @@
+//
+
+#include <iostream>
+
+int main() {
+    int n1 = 0;
+    int n2 = 0;
+    int n3 = 0;
+    std::cout << "Entre com um número: ";
+    std::cin >> n1;
+    std::cout << "Entre com outro número: ";
+    std::cin >> n2;
+    n3 = n1 + n2;
+    std:: cout << "O resultado é: "<< n3 <<"\n";
+    return 0;
+}
